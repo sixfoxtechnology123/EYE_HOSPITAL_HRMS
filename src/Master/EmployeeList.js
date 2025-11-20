92a26ac657dd62c6392ebab371efda451cff6362
Bimal maity <bimalmaity8440@gmail.com>
2025-11-20 12:45:42 +0530

    next

--- src/Master/EmployeeList.js
+++ src/Master/EmployeeList.js
@@ -78,61 +78,61 @@ const EmployeeList = () => {
       <Sidebar/>
     <div className="flex-1 overflow-y-auto">
     <div className="p-3 bg-white shadow-md rounded-md">
-      <div className="bg-green-50 borderborder-green-300 rounded-lg shadow-md p-2 mb-4 flex justify-between items-center">
-        <h2 className="text-xl font-bold text-green-800">Employee List</h2>
+      <div className="bg-blue-50 borderborder-blue-300 rounded-lg shadow-md p-2 mb-4 flex justify-between items-center">
+        <h2 className="text-xl font-bold text-blue-800">Employee List</h2>
         <div className="flex gap-2">
           <BackButton />
           <button
             onClick={() => navigate("/EmployeeMaster")}
-            className="bg-green-600 hover:bg-green-700 text-white px-4 py-1 rounded font-semibold whitespace-nowrap"
+            className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-1 rounded font-semibold whitespace-nowrap"
           >
             Add Employee
           </button>
         </div>
       </div>
 
-      <table className="w-full table-auto border border-green-500">
+      <table className="w-full table-auto border border-blue-500">
         <thead className="bg-gray-200 text-sm">
           <tr>
-            <th className="border border-green-500 px-2 py-1">Sl No</th>
-            <th className="border border-green-500 px-2 py-1">Emp ID</th>
-            <th className="border border-green-500 px-2 py-1">Name</th>
-            {/* <th className="border border-green-500 px-2 py-1">Email</th>
-            <th className="border border-green-500 px-2 py-1">Department</th> */}
-            <th className="border border-green-500 px-2 py-1">Designation</th>
-            <th className="border border-green-500 px-2 py-1">Mobile No.</th>
-            {/* <th className="border border-green-500 px-2 py-1">Employment</th>
-            <th className="border border-green-500 px-2 py-1">Work Location</th>
-            <th className="border border-green-500 px-2 py-1">Contact</th>
-            <th className="border border-green-500 px-2 py-1">Status</th> */}
-            <th className="border border-green-500 px-2 py-1">Action</th>
+            <th className="border border-blue-500 px-2 py-1">Sl No</th>
+            <th className="border border-blue-500 px-2 py-1">Emp ID</th>
+            <th className="border border-blue-500 px-2 py-1">Name</th>
+            {/* <th className="border border-blue-500 px-2 py-1">Email</th>
+            <th className="border border-blue-500 px-2 py-1">Department</th> */}
+            <th className="border border-blue-500 px-2 py-1">Designation</th>
+            <th className="border border-blue-500 px-2 py-1">Mobile No.</th>
+            {/* <th className="border border-blue-500 px-2 py-1">Employment</th>
+            <th className="border border-blue-500 px-2 py-1">Work Location</th>
+            <th className="border border-blue-500 px-2 py-1">Contact</th>
+            <th className="border border-blue-500 px-2 py-1">Status</th> */}
+            <th className="border border-blue-500 px-2 py-1">Action</th>
           </tr>
         </thead>
         <tbody className="text-sm text-center">
           {employees.length ? (
             employees.map((e,index) => (
               <tr key={e._id} className="hover:bg-gray-100 transition">
-                <td className="border border-green-500 px-2 py-1">{index+1}</td>
-                <td className="border border-green-500 px-2 py-1">{e.employeeID}</td>
-                <td className="border border-green-500 px-2 py-1">
+                <td className="border border-blue-500 px-2 py-1">{index+1}</td>
+                <td className="border border-blue-500 px-2 py-1">{e.employeeID}</td>
+                <td className="border border-blue-500 px-2 py-1">
                   {e.firstName} {e.middleName} {e.lastName}
                 </td>
-                {/* <td className="border border-green-500 px-2 py-1">{e.email}</td> */}
+                {/* <td className="border border-blue-500 px-2 py-1">{e.email}</td> */}
 
                 {/* Show ONLY department name (from code)
-                <td className="border border-green-500 px-2 py-1">
+                <td className="border border-blue-500 px-2 py-1">
                   {deptMap[e.departmentID] || e.departmentID}
                 </td> */}
 
-                <td className="border border-green-500 px-2 py-1">
+                <td className="border border-blue-500 px-2 py-1">
                   {e.designationName}
                 </td>
-                <td className="border border-green-500 px-2 py-1">{e.permanentAddress.mobile}</td>
-                {/* <td className="border border-green-500 px-2 py-1">{e.employmentType}</td>
-                <td className="border border-green-500 px-2 py-1">{e.workLocation}</td>
-                <td className="border border-green-500 px-2 py-1">{e.contactNo}</td>
-                <td className="border border-green-500 px-2 py-1">{e.status}</td> */}
-                <td className="border border-green-500 px-2 py-1">
+                <td className="border border-blue-500 px-2 py-1">{e.permanentAddress.mobile}</td>
+                {/* <td className="border border-blue-500 px-2 py-1">{e.employmentType}</td>
+                <td className="border border-blue-500 px-2 py-1">{e.workLocation}</td>
+                <td className="border border-blue-500 px-2 py-1">{e.contactNo}</td>
+                <td className="border border-blue-500 px-2 py-1">{e.status}</td> */}
+                <td className="border border-blue-500 px-2 py-1">
                   <div className="flex justify-center gap-8">
                    <button
                     onClick={() => navigate("/EmployeeMaster", { state: { employee: e, id: e._id } })}
